d3545438d796185c1bcc7886ff32df82f932b6f8
xonaru <46713558+xonaru@users.noreply.github.com>
2019-01-18 12:53:47 +0300

    Update hw02_hard.py

--- lesson02/home_work/hw02_hard.py
+++ lesson02/home_work/hw02_hard.py
@@ -55,66 +55,135 @@ date = '-2.10.3001'
 # Вход: 11
 # Выход: 5 3
 
+#1
+
+equation = 'y = -12x + 11111140.2121'
+x = 2.5
+b = 1
+
+equation = equation.split(' ')
+print(equation)
+
+for num in equation:
+    if num == 'y':
+        continue
+    elif num == '=':
+        continue
+    elif num == '+':
+        operation = True
+        continue
+    elif num == "-":
+        operation = False
+        continue
+    elif num.find('x') >= 0:
+        k = num.split('x')
+        k = k[0]
+        k = float(k)
+    else:
+        b = num
+        b = float(b)
+
+
+if operation:
+    function = k*x + b
+    print('{} = {} * {} + {}'.format(function, k, x, b))
+else:
+    function = k*x - b
+    print('{} = {} * {} - {}'.format(function, k, x, b))
+
+
+
+#2
+
+date = '01.11.1985'
+
+month_max_len = 0
+month_name = ''
+error = 0
+error_d = 0
+error_y = 0
+
+months = [['01','Январь',31],['02','Февраль',28],['03','Март',31],['04','Апрель',30],['05','Май',31],['06','Июнь',30],['07','Июль',31],['08','Август',31],['09','Сентябрь',30],['10','Октябрь',31],['11','Ноябрь',30],['12','Декабрь',31]]
+
+date = date.split('.')
+
+for month in months:
+    if month[0] == date[1]:
+        A = True
+        month_max_len = month[2]
+        month_name = month[1]
+        break
+    else:
+        error = error +1
+if error == 12:
+    print('Ошибка ввода месяца')
+
+if int(date[0]) < 1 | int(date[0]) > month_max_len | len(date[0]) != 2:
+    print('Ошибка ввода дня')
+    error_d = 1
+elif int(date[2]) < 1 | int(date[2]) > 9999 | len(date[2]) != 4:
+    print('Ошибка ввода года')
+    error_y = 1
+elif (error != 12) | (error_y != 1) | (error_d != 1):
+    print('GOOD')
+
 
 #3
+
 import math
 
-max_flat = 2*10**9
-N_Y = True
+max_flat = 2 * 10 ** 9
 
+N_Y = True
+data = []
 start_floor_of_cell = 1
 sum_flats = 0
 cell = 1
-data = []
 position = 0
-remaind = 0
+remains = 0
 floor = 0
 max_flat_in_cell = 0
-number = 0
-count=2
+count = 2
 
 while sum_flats <= max_flat:
-    
-     sum_flats = sum_flats + cell**2
-     start_floor_of_cell = start_floor_of_cell + cell - 1
-     data[len(data):]=[cell,start_floor_of_cell,sum_flats]
-     cell= cell+1
+    sum_flats = sum_flats + cell ** 2
+    start_floor_of_cell = start_floor_of_cell + cell - 1
+    data[len(data):] = [cell, start_floor_of_cell, sum_flats]
+    cell = cell + 1
 
-print(data)
-     
 while N_Y:
     number = int(input('Введите номер квартиры от 1 до 2*10^9 : '))
 
- 
-    while ((number <= 1)|(number >= 2*10**9)):
+    while ((number <= 1) | (number >= 2 * 10 ** 9)):
         print('Ошибка')
         number = int(input('Введите номер квартиры от 1 до 2*10^9 : '))
 
     while number > max_flat_in_cell:
-          max_flat_in_cell = data[count]
-          count = count + 3
+        max_flat_in_cell = data[count]
+        count = count + 3
 
-    cell_of_number = data[count-5]
-    start_floor_of_number = data[count-4]
+    cell_of_number = data[count - 5]
+    start_floor_of_number = data[count - 4]
 
     floor = start_floor_of_number + (cell_of_number - 1 - ((max_flat_in_cell - number) // cell_of_number))
-    remaind = ((max_flat_in_cell - number) % cell_of_number)
+    remains = ((max_flat_in_cell - number) % cell_of_number)
 
-    if remaind > 0:
-        position = cell_of_number - remaind
+    if remains > 0:
+        position = cell_of_number - remains
 
     else:
         position = cell_of_number
 
-    print('На {} этаже {} по счету '.format(floor,position))
+    print('На {} этаже {} по счету слева'.format(floor, position))
     while True:
         answer = input('Повторим N \ Y ? : ')
-        if (answer == 'N')|(answer == 'n'): 
+        if (answer == 'N') | (answer == 'n'):
             N_Y = False
             break
-        elif (answer == 'y')|(answer == 'Y'):
+        elif (answer == 'y') | (answer == 'Y'):
             N_Y = True
             break
         else:
             print('Ошибка')
+
   
