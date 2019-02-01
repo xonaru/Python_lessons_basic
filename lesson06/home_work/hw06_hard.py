8ce180fb423993b1e64241cb1eb9bebb849f8a9b
xonaru <46713558+xonaru@users.noreply.github.com>
2019-02-01 13:52:46 +0300

    ДЗ_6
    hard

--- lesson06/home_work/hw06_hard.py
+++ lesson06/home_work/hw06_hard.py
@@ -10,3 +10,50 @@
 # С использованием классов.
 # Реализуйте классы сотрудников так, чтобы на вход функции-конструктора
 # каждый работник получал строку из файла
+
+import os
+
+
+class Worker:
+    def __init__(self, line):
+
+        name, second_name, pay, post, task = line.split()
+        self.fullname = name + ' ' + second_name
+        self.pay = float(pay)
+        self.task = float(task)
+
+    def calc(self):
+
+        worked = hours.get(self.fullname)
+
+        if worked >= self.task:
+
+            f_pay = self.pay * (2 * (worked / self.task - 1) + 1)
+
+        else:
+
+            f_pay = self.pay * (worked / self.task)
+            
+        return f_pay
+
+
+def new(x):
+
+    name, second_name, hours_w = x.split()
+    return name + ' ' + second_name, float(hours_w)
+
+
+path_w = os.path.join('data', 'workers')
+path_h = os.path.join('data', 'hours_of')
+
+with open(path_w, "r", encoding='UTF-8') as f:
+    f.readline()  # Удаляем заголовок
+    workers = list(map(Worker, f.readlines()))
+
+with open(path_h, "r", encoding='UTF-8') as f:
+    f.readline()  # Уаляем заголовок
+    hours_of = f.readlines()
+
+hours = {person: hours for person, hours in map(new, hours_of)}
+
+print({worker.fullname: round(worker.calc(), 2) for worker in workers})
